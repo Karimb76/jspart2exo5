8be95f06c8af2a5add5d310b9770b915e6398c91
karimb76 <karim.bellet@gmail.com>
2018-10-10 10:21:43 +0200

    maj

---
+++ js.js
@@ -0,0 +1,24 @@
+document.getElementById('image1').addEventListener('mouseover', function chang() {
+document.getElementById('image1').src = 'image1_2.jpg';
+}
+)
+
+document.getElementById('image2').addEventListener('mouseover', function chang() {
+document.getElementById('image2').src = 'image2_2.jpg';
+}
+)
+
+document.getElementById('image3').addEventListener('mouseover', function chang() {
+document.getElementById('image3').src = 'image3_2.jpg';
+}
+)
+
+document.getElementById('image4').addEventListener('mouseover', function chang() {
+document.getElementById('image4').src = 'image4_2.jpg';
+}
+)
+
+document.getElementById('image5').addEventListener('mouseover', function chang() {
+document.getElementById('image5').src = 'image5_2.jpg';
+5
+)
